7c633d56d7d2c63ce420da22afe8b7e6cf0b490f
私募风云 <simufengyun@simufengyundeMacBook-Pro.local>
2020-05-15 13:55:14 +0800

    扩展实例构造器

--- src/components/page/Vue.extend.vue
+++ src/components/page/Vue.extend.vue
@@ -2,11 +2,14 @@
     <div>
         <h1>Vue.extend 扩展实例构造器</h1>
         <hr>
+        <!-- <div id='author'></div> -->
         <author></author>
     </div>
 </template>
 <script>
 import Vue from 'vue'
+// 第一种 创建 挂载
+/* 1、创建注册实例构造器 */
 var AuthorURL = Vue.extend({
   template: '<p><a :href="authorURL">{{authorName}}</a></p>',
   data: function () {
@@ -17,13 +20,22 @@ var AuthorURL = Vue.extend({
   }
 })
 
-new AuthorURL().$mount('author')
+/* 2、挂载扩展实例构造器 */
+// new AuthorURL().$mount('#author')
+/* 第二种：配合全局组件---全局注册组件 */
+Vue.component('author', AuthorURL)
 
 export default {
   name: 'Vue.extend',
   data () {
     return {
     }
+  },
+  beforeCreate: function () {
+    console.log(123)
+  },
+  created: function () {
+    console.log('456')
   }
 }
 </script>
