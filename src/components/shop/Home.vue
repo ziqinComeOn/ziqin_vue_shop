0cc12e56c2d0861ad5a926217c0550294f7a1f50
ziqinComeOn <1047773569@qq.com>
2020-05-21 16:59:58 +0800

    实现后台首页基本布局

--- src/components/shop/Home.vue
+++ src/components/shop/Home.vue
@@ -1,7 +1,15 @@
 <template>
-  <div>
-    <el-button type="info" @click="logout">退出</el-button>
-  </div>
+  <el-container class="home-container">
+      <!-- 头部区域 -->
+    <el-header>Header<el-button type="info" @click="logout">退出</el-button></el-header>
+    <!-- 页面主体区域 -->
+    <el-container>
+        <!-- 侧边栏 -->
+        <el-aside width="200px">Aside</el-aside>
+        <!-- 右侧内容主体 -->
+        <el-main>Main</el-main>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -15,4 +23,17 @@ export default {
 }
 </script>
 <style lang="less" scoped>
+.home-container{
+    height: 100%;
+}
+.el-header{
+    background-color: #373D41;
+}
+.el-aside{
+    background-color: #333744;
+}
+.el-main{
+    background-color: #eaedf1;
+}
+
 </style>
