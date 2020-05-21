0906e6c873a750eb39eb94ff766614886e24d352
ziqinComeOn <1047773569@qq.com>
2020-05-21 15:10:49 +0800

    路由导航守卫控制页面访问权限

--- src/router/index.js
+++ src/router/index.js
@@ -5,11 +5,26 @@ import Home from '@/components/shop/Home'
 
 Vue.use(Router)
 
-export default new Router({
-  // mode: 'history', // mode的两个值history（url不带#）和 hash(url带#)
+const router = new Router({
   routes: [
     {path: '/', redirect: '/login'},
     {path: '/login', name: 'Login', component: Login},
     {path: '/home', name: 'Home', component: Home}
   ]
 })
+
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的页面
+  // from 代表从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  // next() 放行 next('/login') 强制跳转
+  if (to.path === '/login') return next()
+  // 获取token
+  const tokenStr = sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login') // 强制跳转登录页
+  // 此处是放行
+  next()
+})
+
+export default router
