04503a88ffa515a773680a585005457ec3adbaa8
ziqinComeOn <1047773569@qq.com>
2020-05-21 15:16:04 +0800

    实现退出功能

--- src/components/shop/Home.vue
+++ src/components/shop/Home.vue
@@ -1,11 +1,17 @@
 <template>
     <div>
-        Home 组件
+        <el-button type="info" @click="logout">退出</el-button>
     </div>
 </template>
 
 <script>
 export default {
+  methods: {
+    logout () {
+      sessionStorage.clear() // 清空token
+      this.$router.push('/login') // 跳转login页面
+    }
+  }
 }
 </script>
 <style lang="less" scoped>
